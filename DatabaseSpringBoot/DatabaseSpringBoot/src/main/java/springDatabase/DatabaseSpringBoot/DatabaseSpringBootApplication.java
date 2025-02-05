c7362afeb67ec00010f9bcf3c5b726f5afa90dbe
hataraku <geranuser@gmail.com>
2025-02-05 07:40:30 +0700

    Move project to new repository

---
+++ DatabaseSpringBoot/DatabaseSpringBoot/src/main/java/springDatabase/DatabaseSpringBoot/DatabaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package springDatabase.DatabaseSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSpringBootApplication.class, args);
+	}
+
+}
---
+++ DatabaseSpringBoot/DatabaseSpringBoot/src/test/java/springDatabase/DatabaseSpringBoot/DatabaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package springDatabase.DatabaseSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
