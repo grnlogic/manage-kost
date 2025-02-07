11dc011d638d8e1f3a4878a69e79de483f53a207
fannymhr11 <fannymaharanu05@gmail.com>
2025-02-07 15:31:30 +0700

    update FAQ

--- frontend/src/component/user/FAQ.tsx
+++ frontend/src/component/user/FAQ.tsx
@@ -1,5 +1,5 @@
 const Contact = () => {
-    return <h1 className="text-center text-2xl">FAQ</h1>;
+    return <h1 className="text-center text-2xl">ini halaman FAQ</h1>;
   };
   
   export default Contact;
