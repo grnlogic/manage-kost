1f04a118731959fcc8ea8163322f22fabbc22e51
grnlogic <geranuser@gmail.com>
2025-03-05 23:17:32 +0700

    Update database configuration to use Railway env variables

--- frontend/src/component/admin/Edit Info Kamar.tsx
+++ frontend/src/component/admin/Edit Info Kamar.tsx
@@ -1,6 +1,7 @@
 const InfoKamar = () => {
-    return <h1 className="text-center text-2xl">Edit Info kamar admin</h1>;
-  };
-  
-  export default InfoKamar;
-  
+  return (
+    <h1 className="text-center text-2xl">Kelola informasi dan status kamar</h1>
+  );
+};
+
+export default InfoKamar;
